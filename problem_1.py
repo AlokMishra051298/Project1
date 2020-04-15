8a8bf022b7e7df6920e98a3fba3100adf28d3424
AlokMishra051298 <Mishra051298alok@gmail.com>
2020-04-15 20:09:46 +0530

    Modified as per mentor's instruction

--- problem_1.py
+++ problem_1.py
@@ -5,15 +5,57 @@ class Node:
         self.value=value
         self.next=None
         self.prev=None
-#LRU_cache class
-class LRU_Cache(object):
+#doublylinkedlist class I defined only those methods which are needed for LRU_Cache
+class DoublyLinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.no_of_ele=0
+
+    def insert(self, key, value):
+        if self.head is None:
+            node=Node(key, value)
+            self.head=node
+            self.tail=node
+            self.no_of_ele+=1
+            return self.head
+        node=Node(key, value)#create the node
+        node.next=self.head #node points the head
+        self.head.prev=node #now head.prev points to node
+        self.head=node #make to node head
+        self.no_of_ele+=1
+        return self.head
+
+    #update the value of head node
+    def update(self, head, value):
+        self.head.value=value
+
+    def get_tail_key(self):
+        return self.tail.key
+
+    def delete_tail(self):
+        self.tail=self.tail.prev
+        self.tail.next=None
+        self.no_of_ele-=1
+
+    '''This method helps to remove the node which is present
+    in head and tail'''
+    def delete_middle(self, node):
+        ptr=self.head
+        while(ptr!=node):
+            ptr=ptr.next
+        left_ptr=ptr.prev
+        right_ptr=ptr.next
+        left_ptr.next=right_ptr
+        right_ptr.prev=left_ptr
+        self.no_of_ele-=1
+#--------------------------------------------------------------------------
+class LRU_Cache:
 
     def __init__(self, capacity):
-        # Initialize class variables
+                # Initialize class variables
         self.capacity=capacity #the capacity of the cache
-        self.current_len=0 #keeps track of current_length of cache
-        self.head=None # Points the head (first node) of cache
-        self.tail=None #points to the tail(last node) of cache
+        self.cache=DoublyLinkedList() #object of DoublyLinkedList
         self.lookup_dict=dict() #used dictionary because of faster lookups
 
     def get(self, key):
@@ -26,64 +68,39 @@ class LRU_Cache(object):
         return value
 
     def put(self, key, value):
-        # if the key appeared before and present in the cache
+    # if the key appeared before and present in the cache
         if self.lookup_dict.get(key) and self.lookup_dict[key]!=-1:
             node=self.lookup_dict[key]
-            #if that node is at front the we only need to change its value and doesn't need any extra operation
-            if node==self.head:
-                self.head.value=value
+            #if that node is at front the we only need to update its value and doesn't need any extra operation
+            if node==self.cache.head:
+                self.cache.update(value)
                 return
-            #if that node is present at tail so, we only need to detach the last node and
-            #shift the tail left side and insert that node in the front by using insertFirst() function
-            elif node==self.tail:
-                self.tail=self.tail.prev
-                self.tail.next=None
-                self.current_len-=1
-                self.insertFirst(key,value)
+            #if that node is present at tail so,
+            # we only need to detach the last node and
+            # shift the tail left side and insert
+            # that node in the front by using insert() of DoublyLinkedList
+            elif node==(self.cache.tail):
+                self.cache.delete_tail()
+                self.lookup_dict[key]=self.cache.insert(key,value)
                 return
             #else the node is somewhere in cache except the head and tail, so we just remove it from the cache
-            #and place it at head using insertFirst() function
-            left=node.prev #previous node of the node which we want to remove
-            right=node.next #next node of the node which we want to remove
-            left.next=right #step to remove
-            right.prev=left #step to remove
-            self.current_len-=1 #decrease the current length of cache
-            self.insertFirst(key, value) #and insert node at first
+            #and place it at head using insert() of the DoublyLinkedList class.
+            self.cache.delete_middle(node)
+            self.lookup_dict[key]=self.cache.insert(key,value) #and insert node at first
             return
         #if key is newly introduced to cache
-        self.insertFirst(key, value)
-        return
-
-    def insertFirst(self, key, value):
-        #if the cache is empty, insert the node into cache
-        if self.head is None:
-            node=Node(key, value)
-            self.lookup_dict[key]=node
-            self.head=node
-            self.tail=node
-            self.current_len+=1
-            return
         #if the current length of cache is less the maximum capacity, then follow these steps
-        if self.current_len+1<=self.capacity:
-            node=Node(key, value)#create the node
-            node.next=self.head #node points the head
-            self.head.prev=node #now head.prev points to node
-            self.head=node #make to node head
+        if self.cache.no_of_ele+1<=self.capacity:
+            node=self.cache.insert(key, value)
             self.lookup_dict[key]=node #and initialize that key, value pair into lookup_dict
-            self.current_len+=1 #increase the current_len of cache
             return
         #If the cache is at capacity remove the oldest item.
-        index=self.tail.key#index is the key which is now going to detach from cache
+        index=self.cache.get_tail_key()#index is the key which is now going to detach from cache
         self.lookup_dict[index]=-1 # set the value of removed index to -1 so, it is easy to keep track that no node is connected to it
-        self.tail=self.tail.prev # shift tail to previous node
-        self.tail.next=None #detach the tail
-        self.current_len-=1 #decrease the current_length of cache
-        self.insertFirst(key,value)#now simply add the key, value pair by calling the insertFirst() again
-    def show_cache(self):
-        head=self.head
-        while head is not None:
-            print(head.value)
-            head=head.next
+        self.cache.delete_tail() #detach the tail
+        #now simply add the key, value pair by calling insert()
+        self.lookup_dict[key]=self.cache.insert(key, value)
+        return
 
 #-------------------------------------------------------------------------
 #Test cases are here
